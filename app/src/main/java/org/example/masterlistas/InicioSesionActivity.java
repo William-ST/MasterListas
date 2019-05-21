285e4efa9bb1c9311e000af9d5a6efd7ce90fc47
william_ST <wsulcata@everis.com>
2019-05-21 08:49:28 -0500

    advance

---
+++ app/src/main/java/org/example/masterlistas/InicioSesionActivity.java
@@ -0,0 +1,51 @@
+package org.example.masterlistas;
+
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class InicioSesionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio_sesion);
+    }
+
+    public void loguearCheckbox(View v) {
+        CheckBox recordarme = (CheckBox) findViewById(R.id.recordarme);
+        String s = "Recordar datos de usuario: " +
+                (recordarme.isChecked() ? "Sí" : "No");
+        Toast.makeText(this, s, Toast.LENGTH_LONG).show();
+    }
+
+    public void mostrarContraseña(View v) {
+        EditText contraseña = (EditText) findViewById(R.id.contraseña);
+        CheckBox mostrar = (CheckBox) findViewById(R.id.mostrar_contraseña);
+        if (mostrar.isChecked()) {
+            contraseña.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_NORMAL);
+        } else {
+            contraseña.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        }
+    }
+
+    public void acceder(View view) {
+        Intent intent = new Intent(this, ListasActivity.class);
+        startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
+    }
+
+    public void borrarCampos(View view) {
+        EditText usuario = (EditText) findViewById(R.id.usuario);
+        EditText contraseña = (EditText) findViewById(R.id.contraseña);
+        usuario.setText("");
+        contraseña.setText("");
+        usuario.requestFocus();
+    }
+
+}
--- app/src/main/java/org/example/masterlistas/ListasActivity.java
+++ app/src/main/java/org/example/masterlistas/ListasActivity.java
@@ -1,13 +1,101 @@
 package org.example.masterlistas;
 
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
-public class ListasActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListasActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private RecyclerView recycler;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager lManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listas);
+        // Toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.detail_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        // Navigation Drawer
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.drawer_open, R.string.drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        NavigationView navigationView = (NavigationView) findViewById(
+                R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        //Inicializar los elementos
+        List items = new ArrayList();
+        items.add(new Lista(R.drawable.trabajo, "Trabajo", 2));
+        items.add(new Lista(R.drawable.casa, "Personal", 3));
+        // Obtener el Recycler
+        recycler = (RecyclerView) findViewById(R.id.reciclador);
+        recycler.setHasFixedSize(true);
+        // Usar un administrador para LinearLayout
+        // lManager = new GridLayoutManager(this, 2);
+        lManager = new LinearLayoutManager(this);
+        recycler.setLayoutManager(lManager);
+        // Crear un nuevo adaptador
+        adapter = new ListaAdapter(items);
+        recycler.setAdapter(adapter);
+        recycler.addOnItemTouchListener(new RecyclerItemClickListener(ListasActivity.this, new RecyclerItemClickListener.OnItemClickListener() {
+            @Override
+            public void onItemClick(View v, int position) {
+                Toast.makeText(ListasActivity.this, "" + position, Toast.LENGTH_SHORT).show();
+            }
+        }));
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Se presionó el FAB", Snackbar.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.nav_1) {
+            // ...
+        } else if (id == R.id.nav_2) {
+            // ...
+        } else if (id == R.id.nav_3) {
+            // ... } // ...
+        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(
+                R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
